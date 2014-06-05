6189e4ac62d095e65c328a0f4f1d815e4dc96659
Jonathan Tapia <jonathan.tapia@crowdint.com>
2014-06-04 19:45:14 -0500

    Better separation between params

--- app/controllers/spree/quotes_controller.rb
+++ app/controllers/spree/quotes_controller.rb
@@ -1,6 +1,5 @@
 module Spree
   class QuotesController < Spree::StoreController
-    require 'prawn'
     respond_to :pdf
 
     before_action :set_bill_address, only: :create
@@ -11,19 +10,24 @@ module Spree
     end
 
     def create
-      @quote = Quote.find_or_create_by(quote_params)
-      if @quote.valid?
-        render json: @quote.to_json
+      @quote = Quote.find_by(quote_params) || Quote.create(quote_params)
+      if @quote.valid? && @order.bill_address.present?
+        render json: @quote.to_json, status: 200
       else
-        flash[:error] = @quote.errors.messages.values.join(", ")
-        redirect_to :back
+        render json: @quote.errors.messages.values.join(", "), status: 422
       end
     end
 
     private
 
     def quote_params
-      params[:quote].permit(:order_id, :payment_received, :bill_address_attributes)
+      params[:quote].permit(:order_id, :payment_received)
+    end
+
+    def address_params
+      params[:quote].permit(
+        :user_shipping_address,
+        :bill_address_attributes => permitted_address_attributes)
     end
 
     def load_order
@@ -32,13 +36,13 @@ module Spree
 
     def set_bill_address
       if @order.bill_address.blank?
-        if params[:quote][:user_shipping_address] == false
-          @order.build_bill_address(params[:quote][:bill_address_attributes])
+        if address_params[:user_shipping_address] == 'false'
+          @order.build_bill_address(address_params[:bill_address_attributes])
         else
           @order.clone_shipping_address
         end
 
-        @order.save!
+        @order.save
         @order.reload
       end
     end
